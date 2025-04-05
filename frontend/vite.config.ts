1fa84da86823c1b97ad6d1beac12e2dd00cb84ad
AleksanderBrzostowski <olekbrzost@gmail.com>
2025-04-05 23:53:06 +0200

    added todo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import mkcert from 'vite-plugin-mkcert'
 // https://vite.dev/config/
+// TODO: https://github.com/FiloSottile/mkcert/issues/231 fix cert error
 export default defineConfig({
   server: {
     port: 3000
